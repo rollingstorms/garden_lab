62ee2f8d8e67c69824e223195e9627d0dbf4b2eb
Mike Roth <mikerothart@gmail.com>
2026-04-25 22:10:55 -0400

    change default address to 0.0.0.0

--- scripts/dev_core.py
+++ scripts/dev_core.py
@@ -13,6 +13,6 @@ from growlab.core.app.main import app
 if __name__ == "__main__":
     import uvicorn
 
-    host = os.environ.get("GARDEN_LAB_HOST", "127.0.0.1")
+    host = os.environ.get("GARDEN_LAB_HOST", "0.0.0.0")
     port = int(os.environ.get("GARDEN_LAB_PORT", "8000"))
     uvicorn.run(app, host=host, port=port)
